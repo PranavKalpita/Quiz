23ae035cf109f5e984e774e08bd567bd37037ad8
Vaishnavi Mohan <vaishnavi.mohan@kalpitatechnologies.com>
2024-12-19 22:47:08 +0530

    Add Question Upload feature

---
+++ Repository/IQuestionRepository.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace Quiz.WebApi.Repository
+{
+    public interface IQuestionRepository
+    {
+        Task<string> UploadQuestionsAsync(IFormFile file);
+    }
+}
