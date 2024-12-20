fbe18f9cff9ba7cef6af9ba2d91b961e9f24e658
PranavKalpita <pranavkalpitat12@gmail.com>
2024-12-20 09:57:00 +0530

    Upload Portion Completed

--- Repository/QuestionRepository.cs
+++ Repository/QuestionRepository.cs
@@ -1,8 +1,5 @@
 ﻿using System.Data;
-using System.Data.SqlClient;
-using Microsoft.AspNetCore.Http;
-using System.IO;
-using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
 using WebApplicationStoreProcedure.WebApi.DatabaseConfigure;
 
 namespace Quiz.WebApi.Repository
@@ -21,29 +18,32 @@ namespace Quiz.WebApi.Repository
             if (file == null || file.Length == 0 || !file.FileName.EndsWith(".csv"))
                 throw new ArgumentException("Invalid file. Only CSV files are allowed.");
 
-            using (var stream = new StreamReader(file.OpenReadStream()))
+            await using (_databaseConfiguration)
             {
-                var line = await stream.ReadLineAsync(); // Skip header row
-                while ((line = await stream.ReadLineAsync()) != null)
+                await _databaseConfiguration.OpenAsync();
+
+                using (var stream = new StreamReader(file.OpenReadStream()))
                 {
-                    var values = line.Split(',');
+                    var line = await stream.ReadLineAsync(); // Skip header row
 
-                    if (values.Length != 7)
-                        continue; // Skip invalid rows
-
-                    // Parse values
-                    var id = int.Parse(values[0]);
-                    var question = values[1];
-                    var option1 = values[2];
-                    var option2 = values[3];
-                    var option3 = values[4];
-                    var option4 = values[5];
-                    var result = values[6];
-
-                    // Insert into database
-                    using (var connection = new SqlConnection(_databaseConfiguration.ConnectionString))
+                    while ((line = await stream.ReadLineAsync()) != null)
                     {
-                        using (var command = new SqlCommand("sp_InsertQuestion", connection))
+                        var values = line.Split(',');
+
+                        if (values.Length != 7)
+                            continue; // Skip invalid rows
+
+                        // Parse values
+                        var id = int.Parse(values[0]);
+                        var question = values[1];
+                        var option1 = values[2];
+                        var option2 = values[3];
+                        var option3 = values[4];
+                        var option4 = values[5];
+                        var result = values[6];
+
+                        // Insert into database
+                        using (var command = new SqlCommand("sp_InsertQuestion", _databaseConfiguration.GetConnection()))
                         {
                             command.CommandType = CommandType.StoredProcedure;
                             command.Parameters.AddWithValue("@Id", id);
@@ -54,7 +54,6 @@ namespace Quiz.WebApi.Repository
                             command.Parameters.AddWithValue("@Option4", option4);
                             command.Parameters.AddWithValue("@Result", result);
 
-                            connection.Open();
                             await command.ExecuteNonQueryAsync();
                         }
                     }
@@ -63,5 +62,6 @@ namespace Quiz.WebApi.Repository
 
             return "Uploaded successfully";
         }
+
     }
 }
--- obj/Debug/net8.0/Quiz.WebApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/Quiz.WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Quiz.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36ac2ae2ff92601610b7b81f8abe01df7dc3bb65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+643f449f234583ada1390f5e4725b78608727f30")]
 [assembly: System.Reflection.AssemblyProductAttribute("Quiz.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Quiz.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
